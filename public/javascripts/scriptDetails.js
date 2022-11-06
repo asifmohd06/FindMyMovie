2c39cd7daa0ae4a1327dfd36f89e844955fd6af6
asif mohd <itsafakemail400@gmail.com>
2022-11-06 18:24:17 +0530

    changed button effects

--- public/javascripts/scriptDetails.js
+++ public/javascripts/scriptDetails.js
@@ -37,9 +37,9 @@ for (let btn of tbtn) {
                 size.innerHTML = element.size
                 seeds.innerHTML = element.seeds
 
-                magnet.innerHTML = `<a href="magnet:?xt=urn:btih:${element.hash}&dn=${title}-${element.quality}-${element.size}-YTS.MX&tr=http://track.one:1234/announce&tr=udp://track.two:80&tr=udp://open.demonii.com:1337/announce&tr=udp://tracker.openbittorrent.com:80&tr=udp://tracker.coppersurfer.tk:6969&tr=udp://glotorrents.pw:6969/announce&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://torrent.gresille.org:80/announce&tr=udp://p4p.arenabg.com:1337&tr=udp://tracker.leechers-paradise.org:6969"><button class="download__btn text__dim">Magnet</button></a>`
+                magnet.innerHTML = `<a class="link__effect" href="magnet:?xt=urn:btih:${element.hash}&dn=${title}-${element.quality}-${element.size}-YTS.MX&tr=http://track.one:1234/announce&tr=udp://track.two:80&tr=udp://open.demonii.com:1337/announce&tr=udp://tracker.openbittorrent.com:80&tr=udp://tracker.coppersurfer.tk:6969&tr=udp://glotorrents.pw:6969/announce&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://torrent.gresille.org:80/announce&tr=udp://p4p.arenabg.com:1337&tr=udp://tracker.leechers-paradise.org:6969"><div class="download__btn ">Magnet</div></a>`
 
-                torrentFile.innerHTML = `<a href="https://yts.mx/torrent/download/${element.hash}" rel="nofollow" title="Download  ${title} ${element.quality} Torrent"><button class="download__btn text__dim">File</button></a>`
+                torrentFile.innerHTML = `<a class="link__effect" href="https://yts.mx/torrent/download/${element.hash}" rel="nofollow" title="Download  ${title} ${element.quality} Torrent"><div class="download__btn ">File</div></a>`
             }
         });
 
